49ad0d66ec4a1a04ea30cee8798494a4c3126f90
Krishna Sharma <sharmakriss985@gmail.com>
2026-01-06 19:21:47 +0530

    test

--- syntax/02_advanced/12_iterators.py
+++ syntax/02_advanced/12_iterators.py
@@ -41,7 +41,7 @@ print(next(iterator))  # 3
 iterator2 = iter(numbers)
 
 print(next(iterator2))    # 1
-print(next(iterator))     # 2
+print(next(iterator2))    # 2
 
 # The iterator remembers where it stopped.
 # You cannot reset it unless you create a new iterator.
@@ -93,3 +93,78 @@ for num in counter2:
 # ✔ StopIteration ends iteration
 # ✔ for-loop uses iter() + next() internally
 # ✔ Iterator remembers state
+
+# ==============================
+# ITERATORS — Test (QUESTIONS)
+# ==============================
+
+# Q1
+# For each object below, print whether it has:
+# 1) __iter__
+# 2) __next__
+#
+# Objects:
+# - list                            -- has __iter__
+# - list iterator                   -- has __next__
+# - set                             -- has __iter__
+# - set iterator                    -- has __next__
+# - dict                            -- has __iter__ 
+# - dict iterator                   -- has __next__
+
+# Q2
+# Create ONE iterator from a list.
+# Use a first for-loop to print all elements.
+# Use a second for-loop on the SAME iterator to prove exhaustion.
+
+print()
+a = [1, 2, 3]
+b = iter(a)
+for x in b:
+    print(x)
+for y in b:
+    print(y)
+
+# Q3
+# Without using a for-loop:
+# Iterate completely through [100, 200, 300]
+# Use iter(), next(), and try/except StopIteration.
+
+p = [100, 200, 300]
+q = iter(p)
+
+print()
+while True:
+    try:
+        print(next(q))
+    except StopIteration:
+        print("Iteration finished")
+        break
+
+# Q4
+# Create an iterator.
+# Prove using code that:
+# iter(iterator) is iterator
+
+print()
+a = [1, 2, 3]              # iterable 
+b = iter(a)                # iterator
+print(next(b))             
+c = iter(b)                # iter of iterator
+print(next(c))             # iter of iterator worms as same as iterator  (shows same behaviour)
+d = b is c                 # iter(iterator) is iterator (Shows same identtity)
+print(d) 
+
+# Q5
+# Create a dictionary with at least two key-value pairs.
+# Iterate over it using a for-loop.
+# Print exactly what is produced by default.
+
+print()
+student = {
+    "Name": "Kris",
+    "Age": 19,
+    "is_codes" : True
+}
+
+for x in student:
+    print(x)
